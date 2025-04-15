8aad9ddf4cb952f6bcdb3ca64de63ed27d36e543
Phcostaa16 <169086747+Phcostaa16@users.noreply.github.com>
2025-04-14 21:57:11 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "127.0.0.1";
-$database = "mydb"; 
+$database = "RestauranteDB"; 
 $username = "root"; 
 $password = ""; 
 
@@ -10,23 +10,19 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully";
-
-
-// Processamento do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $senha = $_POST["senha"];
     $confirmarsenha = $_POST["confirmarsenha"];
+    $nome = $_POST["nome"]; 
+    $telefone = $_POST["telefone"];
 
-    // Validação dos dados
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Email inválido.";
     } elseif ($senha !== $confirmarsenha) {
         echo "As senhas não coincidem.";
     } else {
-        // Consulta para verificar se o usuário já existe
-        $stmt = $conn->prepare("SELECT * FROM usuário WHERE email = ?");
+        $stmt = $conn->prepare("SELECT * FROM Usuarios WHERE Email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -34,29 +30,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($result->num_rows > 0) {
             echo "Usuário já existe.";
         } else {
-            // Criptografa a senha
             $hash_senha = password_hash($senha, PASSWORD_BCRYPT);
 
-            // Insere o usuário no banco de dados
-            $stmt = $conn->prepare("INSERT INTO usuário (email, senha) VALUES (?, ?)");
-            $stmt->bind_param("ss", $email, $hash_senha);
+            $stmt = $conn->prepare("INSERT INTO Usuarios (Nome, Telefone, Email, Senha) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssss", $nome, $telefone, $email, $hash_senha);
 
             if ($stmt->execute()) {
                 echo "Cadastro realizado com sucesso!";
-                header("Location: login.html"); // Redireciona automaticamente para login.html
-                exit(); // Encerra o script para garantir que o redirecionamento funcione
+                header("Location: login.html");
+                exit();
             } else {
                 echo "Erro ao cadastrar usuário.";
             }
-            
-            
-               
-            }
         }
-
         $stmt->close();
     }
-
+}
 
 $conn->close();
 ?>
