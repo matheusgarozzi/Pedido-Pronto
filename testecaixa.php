d4c312a17a4c8783c60bcfef80dbb8f599aad6ac
Phcostaa16 <169086747+Phcostaa16@users.noreply.github.com>
2025-05-28 20:54:24 -0300

    Delete testecaixa.php

--- testecaixa.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'funcoes.php';
-atualizarCaixa('abrir', 100);
-echo "Caixa aberto manualmente.<br>";
-
-$dados = buscarStatusCaixa();
-echo "<pre>";
-print_r($dados);
-echo "</pre>";
