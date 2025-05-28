a30b9ee4a99f365d5c5b3b400f18ce786c420597
matheusgarozzi <matheusgwachholz@hotmail.com>
2025-05-28 20:50:21 -0300

    Delete Pedido-Pronto-main directory

--- Pedido-Pronto-main/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "127.0.0.1";
-$database = "PedidoProntoDB"; 
-$username = "root"; 
-$password = ""; 
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
